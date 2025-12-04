3359b3eb0b3dfc061e21fbcf954a56d8b1902c44
RodionClepa <raduclepa@gmail.com>
2025-12-04 09:23:14 +0200

    feat: add express and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello!")
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!!');
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
+}
+
+startServer();
